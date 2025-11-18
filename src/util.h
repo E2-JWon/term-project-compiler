8cd885fc77b266d3d7418e29f22255ac2ba5460c
jonghoon_kim <kidjohn0306@naver.com>
2025-11-18 11:58:00 +0900

    Update util.h

--- src/util.h
+++ src/util.h
@@ -6,8 +6,18 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 
+#include "parse.h"
 #include "globals.h"
 
-void printToken(TokenType token, const char *tokenString);
 
+void printToken(TokenType token, const char *tokenString);
+void printTree(TreeNode * tree);
+
+TreeNode* newStmtNode(StmtKind kind);
+TreeNode* newExpNode(ExpKind kind);
+TreeNode* newDeclNode(DeclKind kind);
+
+/* 토큰 문자열 복사를 위한 헬퍼 함수 */
+char* copyString(char* s);
 #endif
+
