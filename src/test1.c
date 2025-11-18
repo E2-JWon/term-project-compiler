329465221dc35cb1348f37d8a6e6b42c24a5b73a
jonghoon_kim <kidjohn0306@naver.com>
2025-11-18 11:59:48 +0900

    Create test1.c

---
+++ src/test1.c
@@ -0,0 +1,16 @@
+/* fact test program */
+int fact(int x)
+{
+    if (x > 1)
+        return x * fact(x - 1);
+    else
+        return 1;
+}
+
+void main(void)
+{
+    int x;
+    x = input();
+    if (x > 0)
+        output(fact(x));
+}
